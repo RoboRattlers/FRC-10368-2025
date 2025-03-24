df1d1aa5994ccf3b0fda43ee38087b4c08acd38e
ewyzzia <giikae127@gmail.com>
2025-03-24 08:14:23 -0700

    Add limits, add slew rate limiter, and use on-RIO PID for pivot and elevator

--- src/main/java/frc/robot/subsystems/swervedrive/ElevatorSubsystem.kt
+++ src/main/java/frc/robot/subsystems/swervedrive/ElevatorSubsystem.kt
@@ -1,8 +1,16 @@
 package frc.robot.subsystems.swervedrive
 
+import com.revrobotics.spark.SparkBase
+import com.revrobotics.spark.SparkLowLevel
+import com.revrobotics.spark.SparkMax
+import com.revrobotics.spark.config.EncoderConfig
+import com.revrobotics.spark.config.SoftLimitConfig
+import com.revrobotics.spark.config.SparkBaseConfig
+import com.revrobotics.spark.config.SparkMaxConfig
 import com.thethriftybot.ThriftyNova
-import com.thethriftybot.ThriftyNova.PIDSlot
+import edu.wpi.first.math.MathUtil.clamp
 import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.filter.SlewRateLimiter
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -11,45 +19,70 @@ import kotlin.math.sign
 
 class ElevatorSubsystem : SubsystemBase() {
 
-    val ELEVATOR_TOP_ENCODER_VALUE = 1562.0
-    val kP = 0.02
+    val ELEVATOR_MAX_ENCODER_VALUE = 1750.0
+    val kP = 20.0
     val kI = 0.0
     val kD = 0.0
-    val kFF = 1.35/2.0
-    val kS = 0.125
+    val kFF = 0.2//0.8
+    val kS = (0.8 - 0.4)/2.0
     val pidController = PIDController(kP, kI, kD)
+    var currentHeight = 0.0
+        private set
+    var setpoint = 0.0
+        private set
+    val slewRateLimiter = SlewRateLimiter(1.3)
 
-    val motorTop = ThriftyNova(CanId.ELEVATOR_TOP)
-        .setInversion(false)
-        .setBrakeMode(true)
-        .setMaxCurrent(ThriftyNova.CurrentType.STATOR, 40.0)
-        .setMaxOutput(0.8)
-        .setSoftLimits(0.0, ELEVATOR_TOP_ENCODER_VALUE)
-        .useEncoderType(ThriftyNova.EncoderType.INTERNAL)
-    val motorBottom = ThriftyNova(CanId.ELEVATOR_BOTTOM)
-        .setInversion(false)
-        .setBrakeMode(true)
-        .setMaxCurrent(ThriftyNova.CurrentType.STATOR, 40.0)
-        .setMaxOutput(0.8)
-        .setSoftLimits(0.0, ELEVATOR_TOP_ENCODER_VALUE)
+    val motorTop = SparkMax(CanId.ELEVATOR_TOP, SparkLowLevel.MotorType.kBrushless)
+    val motorConfig = SparkMaxConfig()
+        .smartCurrentLimit(40)
+        .idleMode(SparkBaseConfig.IdleMode.kBrake)
+        .apply(
+            SoftLimitConfig()
+                .forwardSoftLimit(ELEVATOR_MAX_ENCODER_VALUE)
+                .forwardSoftLimitEnabled(true)
+                .reverseSoftLimit(0.0)
+                .reverseSoftLimitEnabled(true)
+        )
+        .apply(
+            EncoderConfig()
+                .positionConversionFactor(42.0)
+        )
+    val motorBottom = SparkMax(CanId.ELEVATOR_BOTTOM, SparkLowLevel.MotorType.kBrushless)
 
     init {
-        //motorBottom.follow(CanId.ELEVATOR_TOP)
+        motorTop.configure(
+            motorConfig,
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
+        motorBottom.configure(
+            SparkMaxConfig()
+                .apply(motorConfig)
+                .follow(motorTop),
+            SparkBase.ResetMode.kResetSafeParameters,
+            SparkBase.PersistMode.kPersistParameters
+        )
+        SmartDashboard.putNumber("Elevator FF", 0.0)
     }
 
     override fun periodic() {
-        val encoderPos = motorBottom.positionInternal
+        val encoderPos = motorTop.encoder.position
+        val heightFraction = encoderPos/ELEVATOR_MAX_ENCODER_VALUE
+        currentHeight = heightFraction
         SmartDashboard.putNumber("Elevator Encoder", encoderPos)
+        SmartDashboard.putNumber("Elevator Fraction", heightFraction)
         SmartDashboard.putNumber("Elevator Setpoint", pidController.setpoint)
-        SmartDashboard.putNumber("Elevator Current", motorBottom.statorCurrent)
-        val voltage = pidController.calculate(encoderPos) + kFF + kS * sign(pidController.setpoint - encoderPos)
-        motorTop.setVoltage(voltage)
-        motorBottom.setVoltage(voltage)
+        SmartDashboard.putNumber("Elevator Current", motorTop.outputCurrent)
+        pidController.setpoint = slewRateLimiter.calculate(setpoint)
+        val voltage = pidController.calculate(heightFraction) +
+                kFF
+        + kS * sign(pidController.setpoint - encoderPos)
+        motorTop.setVoltage(clamp(voltage, -12.0, 12.0) )
     }
 
     fun toPosCommand(pos: Double): Command {
         return runOnce {
-            pidController.setpoint = pos * ELEVATOR_TOP_ENCODER_VALUE
+            setpoint = pos
         }
     }
 
--- src/main/java/frc/robot/subsystems/swervedrive/PivotSubsystem.kt
+++ src/main/java/frc/robot/subsystems/swervedrive/PivotSubsystem.kt
@@ -2,36 +2,72 @@ package frc.robot.subsystems.swervedrive
 
 import com.thethriftybot.ThriftyNova
 import com.thethriftybot.ThriftyNova.PIDSlot
+import edu.wpi.first.math.MathUtil.clamp
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.filter.SlewRateLimiter
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.Constants
 
 class PivotSubsystem : SubsystemBase() {
 
-    val motor = ThriftyNova(Constants.CanId.PIVOT)
+    private val motor = ThriftyNova(Constants.CanId.PIVOT)
         .setInversion(false)
         .setBrakeMode(true)
-        .setMaxCurrent(ThriftyNova.CurrentType.STATOR, 10.0)
+        .setMaxCurrent(ThriftyNova.CurrentType.STATOR, 35.0)
         .setMaxOutput(1.0)
-        .setSoftLimits(0.0, 900.0)
+        .setSoftLimits(0.0, 1400.0)
         .useEncoderType(ThriftyNova.EncoderType.INTERNAL)
+
+    val encoderMutiplier = (56.0/14.0) / (26.0/10.0)
+
+    val kP = 0.02
+    val kI = 0.0
+    val kD = 0.0
+    val kFF = 0.0
+    val kS = (0.8 - 0.4)/2.0
+    val pidController = PIDController(kP, kI, kD)
+    var setpoint = 0.0
+        private set
+    val slewRateLimiter = SlewRateLimiter(2000.0)
+
+    var usePid = true
+    var lowerBound = 400.0
+    var upperBound = 900.0
 
     init {
         motor.pid0.setP(0.001)
             .setI(0.0)
             .setD(0.0)
-        motor.usePIDSlot(PIDSlot.SLOT0)
+        //motor.usePIDSlot(PIDSlot.SLOT0)
     }
 
     override fun periodic() {
         SmartDashboard.putNumber("Pivot Encoder", motor.positionInternal)
         SmartDashboard.putNumber("Pivot Current", motor.statorCurrent)
+
+        val setpointBeforeClamp = setpoint
+        setpoint = clamp(setpoint, lowerBound, upperBound)
+        if (setpointBeforeClamp != setpoint) {
+            slewRateLimiter.reset(setpoint)
+        }
+        pidController.setpoint = slewRateLimiter.calculate(setpoint)
+        val voltage = pidController.calculate(motor.positionInternal * encoderMutiplier) +
+                kFF
+        //+ kS * sign(pidController.setpoint - encoderPos)
+        motor.setVoltage(clamp(voltage, -12.0, 12.0) )
+        for (error in motor.errors) {
+            println(error)
+        }
+        motor.clearErrors()
+
     }
 
     fun toPosCommand(pos: Double): Command {
         return runOnce {
-            motor.position = pos
+            setpoint = pos
         }
     }
 
